8ae4a1c8fe3ab8da9c3fe2bfdc5a513a1a47c6db
Guilherme Faustino <guilherme.faustino_thera@prestador.globo>
2021-01-16 19:07:33 -0300

    add title header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,25 @@
+import { IonPage, IonRow } from "@ionic/react";
 import * as React from "react";
 
+import * as S from "./styles";
+
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <IonPage>
+      <S.StyledContent
+        color="content-background-light"
+        className="ion-padding ion-text-center"
+      >
+        <div>
+          <S.Header>
+            <S.TitleSearch>
+              <span>Olá Hugo,</span> Buscar Matéria
+            </S.TitleSearch>
+          </S.Header>
+        </div>
+      </S.StyledContent>
+    </IonPage>
+  );
 };
 
 export default Home;
